09706d1c7a53f4fa309be1df07df454b58f58a61
Andre Parada <andreparada98@gmail.com>
2022-05-20 16:51:12 -0400

    Adicionando Rotas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')      // Importação do Express
 const db = require('./config/dbConnect')    // Importação do Banco na nuvem
 const cors = require('cors')            // Importação do Cors
+const routes = require('./routes')
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))  // caso aconteça erro, dirá no terminal.
 db.once("open", () => {
@@ -13,5 +14,7 @@ app.use(
     cors(),             //Habilita o cors
     express.json()      //Diz para o Express que usaremos a estrutura Json
 )
+
+routes(app)
 
 module.exports = app
