3b7435a85fec41c67f05fc9c32bba9fd8f04f476
Andre Parada <andreparada98@gmail.com>
2022-05-20 14:20:57 -0400

    Adicionando Exportacao de APP

--- config/dbConnect.js
+++
@@ -1,6 +0,0 @@
-module.exports = () => {
-    const { default: mongoose } = require("mongoose");  // importação do mongoose
-mongoose.connect("mongodb+srv://andre:123@agendamentocovid19.edb2x.mongodb.net/AgendamentoCovid19") // conexão com o Banco
-let db = mongoose.connection;
-    return db;
-}
